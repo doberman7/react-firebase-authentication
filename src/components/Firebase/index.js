f2f10e6b1837a45e82e0bc6d7ead5614a3f4ae0e
doberman7 <ivanrubioangeles@gmail.com>
2021-01-21 16:32:30 -0600

    import  {FirebaseContext}  from ./components/Firebase/index

--- src/components/Firebase/index.js
+++ src/components/Firebase/index.js
@@ -1,9 +1,6 @@
-import React from 'react';
-
-const Firebase = () => (
-  <div>
-    <h1>Firebase</h1>
-  </div>
-);
-
+import FirebaseContext from './context';
+import Firebase from './firebase';
+console.log(FirebaseContext);
 export default Firebase;
+
+export { FirebaseContext };
