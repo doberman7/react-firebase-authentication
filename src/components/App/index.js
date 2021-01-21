b4ae06b4517c58553f11fb2cb0799a38cc515c87
doberman7 <ivanrubioangeles@gmail.com>
2021-01-20 19:17:24 -0600

    add routes

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -1,9 +1,21 @@
 import React from 'react';
+import { BrowserRouter as Router } from 'react-router-dom';
+
+import Navigation from '../Navigation';
 
 const App = () => (
-  <div>
-    <h1>App</h1>
-  </div>
+  <Router>
+    <Navigation />
+  </Router>
 );
 
 export default App;
+// import React from 'react';
+
+// const App = () => (
+//   <div>
+//     <h1>App</h1>
+//   </div>
+// );
+
+// export default App;
