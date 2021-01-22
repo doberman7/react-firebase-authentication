d8b22c7ebc9a414945ba3810a4d0fe4891a1d0ce
ivan <ivanrubioangeles@gmail.com>
2021-01-22 17:35:18 -0600

    Form Fields #

--- src/components/SignUp/index.js
+++ src/components/SignUp/index.js
@@ -1,9 +1,92 @@
-import React from 'react';
+import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
 
-const SignUp = () => (
+import * as ROUTES from '../../constants/routes';
+
+const SignUpPage = () => (
   <div>
     <h1>SignUp</h1>
+    <SignUpForm />
   </div>
 );
+//initialize the state of the component. It will capture user information such as username, email, and password. There will be a second password field/state for password confirmation.initialize the state of the component. It will capture user information such as username, email, and password. There will be a second password field/state for password confirmation.The state is initialized by an object destructuring. This way, we can use the initial state object to reset the state after a successful sign-up.
+const INITIAL_STATE = {
+  username: '',
+  email: '',
+  passwordOne: '',
+  passwordTwo: '',
+  error: null,
+};
 
-export default SignUp;
+class SignUpForm extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = { ...INITIAL_STATE };
+
+  }
+
+  onSubmit = event => {
+
+  }
+  //he input fields need to update the local state of the component by using an onChange handler
+  onChange = event => {
+    this.setState({ [event.target.name]: event.target.value });
+
+  };
+
+  render() {
+    const {
+      username,
+      email,
+      passwordOne,
+      passwordTwo,
+      error,
+    } = this.state;
+    return (
+      <form onSubmit={this.onSubmit}>
+        <input
+          name="username"
+          value={username}
+          onChange={this.onChange}
+          type="text"
+          placeholder="Full Name"
+        />
+        <input
+          name="email"
+          value={email}
+          onChange={this.onChange}
+          type="text"
+          placeholder="Email Address"
+        />
+        <input
+          name="passwordOne"
+          value={passwordOne}
+          onChange={this.onChange}
+          type="password"
+          placeholder="Password"
+        />
+        <input
+          name="passwordTwo"
+          value={passwordTwo}
+          onChange={this.onChange}
+          type="password"
+          placeholder="Confirm Password"
+        />
+        <button type="submit">Sign Up</button>
+
+        {error && <p>{error.message}</p>}
+      </form>
+    );
+  }
+}
+
+const SignUpLink = () => (
+  <p>
+    Don't have an account? <Link to={ROUTES.SIGN_UP}>Sign Up</Link>
+  </p>
+);
+
+export default SignUpPage;
+
+export { SignUpForm, SignUpLink };
