d7835f00738a59716cdd61ceb023ef2cba3514d7
doberman7 <ivanrubioangeles@gmail.com>
2021-04-09 18:58:02 -0500

    Exporting the Higher-Order Component

--- src/components/Session/index.js
+++ src/components/Session/index.js
@@ -1,5 +1,6 @@
 import AuthUserContext from "./context";
 //export the higher-order component <withAuthentication>, so that it can be used in the App component afterward:
 import withAuthentication from "./withAuthentication";
+import withAuthorization from './withAuthorization';
 
-export { AuthUserContext, withAuthentication };
+export { AuthUserContext, withAuthentication, withAuthorization };
