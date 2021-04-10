97cd9d6f6a499126598907b9d7526cc12f4a82a3
doberman7 <ivanrubioangeles@gmail.com>
2021-04-09 19:23:23 -0500

    Exporting the Higher-Order Component to account

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -1,9 +1,14 @@
 import React from 'react';
+// enhance the HomePage component with the higher-order component and define the authorization condition for it:
+import { withAuthorization } from '../Session';
 
-const Home = () => (
+const HomePage = () => (
   <div>
-    <h1>Home</h1>
+    <h1>Home Page</h1>
+    <p>The Home Page is accessible by every signed in user.</p>
   </div>
 );
+//define the authorization condition for it:
+const condition = authUser => !!authUser;
 
-export default Home;
+export default withAuthorization(condition)(HomePage);
